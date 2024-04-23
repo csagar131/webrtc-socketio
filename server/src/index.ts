320f95d1b0c7e532d3055126c7e5039f778275b8
Sagar Mochi <sagar.mochi@zopper.com>
2024-04-23 18:32:02 +0530

    .

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import serverconfig from "./config/serverconfig";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+  console.log("user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+server.listen(serverconfig.PORT, () => {
+  console.log(`connection established ${serverconfig.PORT}`);
+});
